9ec37ea3476e3d84e28410ff98eb6167e27341b9
scopolia <126039257+scopolia@users.noreply.github.com>
2023-11-29 22:46:19 -0300

    up

---
+++ BACK/JS_BACK/header.js
@@ -0,0 +1,5 @@
+document.getElementById("header").innerHTML= `
+<nav class="menu">
+            <a href="./tablaContactos.html">Tabla</a>
+            <a href="./ingresarContacto.html">Ingresar contacto</a>
+</nav>`
