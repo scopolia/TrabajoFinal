3236e86e7f6a7e4f7dff97dc61c46799acf1bb04
scopolia <126039257+scopolia@users.noreply.github.com>
2023-12-03 15:29:49 -0300

    x

--- FRONTEND_DANIEL/js/header.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("header").innerHTML= `
-<nav class="menu">
-            <a href="../index.html">Inicio</a>
-            <a href="productos.html">Tabla</a>
-            <a href="ingresar_producto.html">Registrar producto</a>
-</nav>
-`
